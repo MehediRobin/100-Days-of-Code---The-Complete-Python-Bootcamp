652c1e3f27d5f4bd5003388107e0b264ee970862
Mehedi Robinm <robinicage@gmail.com>
2024-12-09 18:50:14 +0600

    Day 1

---
+++ Day 1 - Print/main.py
@@ -0,0 +1,9 @@
+#greetings
+print("Welcome to the Band Namge Generator.")
+
+#taking user inputs
+location = input("Where do you live in ?")
+pet_name = input("What is the name of the pet? ")
+
+#suggesting a band name
+print("Your band name could be " + location + " " + pet_name)
